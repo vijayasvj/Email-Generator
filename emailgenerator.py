78b32080eb85e9d532346fc752edad1c80b29ee1
vijayasvj <65366176+vijayasvj@users.noreply.github.com>
2022-09-04 14:41:41 +0530

    Update emailgenerator.py

--- emailgenerator.py
+++ emailgenerator.py
@@ -42,32 +42,34 @@ st.markdown("""
 * Anxiety of writing a **professional sounding** email (or email with any writing style) is removed as the GPT3 Language model used is trained from a variety of many different internet sources
 """)
 
-st.markdown("# Generate Email")
+if key_input:
+    
+    st.markdown("# Generate Email")
 
 
-with st.form(key="form"):
-    prompt = st.text_input("Describe the Kind of Email you want to be written.")
-    st.text(f"(Example: Write me a professional sounding email to my boss)")
+    with st.form(key="form"):
+        prompt = st.text_input("Describe the Kind of Email you want to be written.")
+        st.text(f"(Example: Write me a professional sounding email to my boss)")
 
-    start = st.text_input("Begin writing the first few or several words of your email:")
+        start = st.text_input("Begin writing the first few or several words of your email:")
 
-    slider = st.slider("How many characters do you want your email to be? ", min_value=64, max_value=750)
-    st.text("(A typical email is usually 100-500 characters)")
+        slider = st.slider("How many characters do you want your email to be? ", min_value=64, max_value=750)
+        st.text("(A typical email is usually 100-500 characters)")
 
-    submit_button = st.form_submit_button(label='Generate Email')
+        submit_button = st.form_submit_button(label='Generate Email')
 
-    if submit_button:
-        with st.spinner("Generating Email..."):
-            output = generate_email(key_input, prompt, start)
-        st.markdown("# Email Output:")
-        st.subheader(start + output)
+        if submit_button:
+            with st.spinner("Generating Email..."):
+                output = generate_email(key_input, prompt, start)
+            st.markdown("# Email Output:")
+            st.subheader(start + output)
 
-        st.markdown("____")
-        st.markdown("# Send Your Email")
-        st.subheader("You can press the Generate Email Button again if you're unhappy with the model's output")
-        
-        st.subheader("Otherwise:")
-        st.text(output)
-        url = "https://mail.google.com/mail/?view=cm&fs=1&to=&su=&body=" + replace_spaces_with_pluses(start + output)
+            st.markdown("____")
+            st.markdown("# Send Your Email")
+            st.subheader("You can press the Generate Email Button again if you're unhappy with the model's output")
 
-        st.markdown("[Click me to send the email]({})".format(url))
+            st.subheader("Otherwise:")
+            st.text(output)
+            url = "https://mail.google.com/mail/?view=cm&fs=1&to=&su=&body=" + replace_spaces_with_pluses(start + output)
+
+            st.markdown("[Click me to send the email]({})".format(url))
